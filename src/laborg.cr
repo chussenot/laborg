0d6a36d72e8c84c46e08be443bced912f937e34b
Clement Hussenot <chussenot@vente-privee.com>
2018-12-13 14:09:18 +0100

    Just a first draft

--- src/laborg.cr
+++ src/laborg.cr
@@ -1,4 +1,6 @@
 require "gitlab"
+require "clim"
+require "json"
 
 exit unless ENV["GITLAB_TOKEN"]?
 exit unless ENV["GITLAB_HOST"]?
@@ -9,9 +11,35 @@ module Laborg
     def initialize
       endpoint = "#{ENV["GITLAB_HOST"]}/api/v4"
       token = ENV["GITLAB_TOKEN"]
+      @client = Gitlab.client(endpoint, token)
+    end
+
+    def plan
+      puts @client.groups.to_json
+    end
+  end
+  class Cli < Clim
+    main do
+      desc "Laborg CLI."
+      run do |opts, args|
+        opts.help # => help string
+      end
+      sub "plan" do
+        desc "Generate an execution plan"
+        run do |opts, args|
+          core = Laborg::Main.new
+          core.plan
+          puts "sub_command run!!"
+        end
+      end 
+      sub "apply" do
+        desc "Builds or Changes the first level groups"
+        run do |opts, args|
+          puts "sub_command run!!"
+        end
+      end 
     end
   end
 end
 
-Laborg::Main.new
-
+Laborg::Cli.start(ARGV)
