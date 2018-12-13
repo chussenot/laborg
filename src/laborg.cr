2b22bdc706d4a0936869e1c71fa4ebeb85b750d6
Clement Hussenot <chussenot@vente-privee.com>
2018-12-13 14:46:17 +0100

    Read all the group entries

--- src/laborg.cr
+++ src/laborg.cr
@@ -7,6 +7,26 @@ exit unless ENV["GITLAB_HOST"]?
 
 module Laborg
   VERSION = "0.1.0"
+
+  alias Groups = Array(Group)
+  class Group
+    include JSON::Serializable
+    property id : Int32
+    property web_url : String
+    property name : String
+    property path : String
+    property description : String
+    property visibility : String
+    property lfs_enabled : Bool
+    property avatar_url : String?
+    property request_access_enabled : Bool
+    property full_name : String
+    property full_path : String
+    property parent_id : Int32?
+    property ldap_cn : Nil
+    property ldap_access : Nil
+  end
+
   class Main
     def initialize
       endpoint = "#{ENV["GITLAB_HOST"]}/api/v4"
@@ -15,7 +35,18 @@ module Laborg
     end
 
     def plan
-      @client.groups.to_json
+      groups = Groups.new
+      i = 0
+      loop do
+        result = @client.groups({"page" => i})
+        if result.size == 0
+          break
+        else
+          groups = groups + Groups.from_json(result.to_json)
+        end
+        i = i + 1
+      end
+      groups
     end
 
     def apply
@@ -36,14 +67,14 @@ module Laborg
           core = Laborg::Main.new
           puts core.plan
         end
-      end 
+      end
       sub "apply" do
         desc "Builds or Changes the first level groups"
         run do |opts, args|
           core = Laborg::Main.new
           core.apply
         end
-      end 
+      end
     end
   end
 end
